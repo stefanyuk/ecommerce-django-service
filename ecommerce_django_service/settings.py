5c11e29dac4c1144e1ead184e4a239353ba0a66d
sstefanyuk <stefanyuk.andriyigor@gmail.com>
2023-08-03 12:34:43 +0200

    configured docker-compose for backend and frontend. Added dependency for CORS handling

--- ecommerce_django_service/settings.py
+++ ecommerce_django_service/settings.py
@@ -27,8 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
+    "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = "ecommerce_django_service.urls"
