f5a485caf2f7f8260409bfd0fde140425268dc8b
sstefanyuk <stefanyuk.andriyigor@gmail.com>
2023-08-17 11:32:38 +0200

    added call commands to demo to setup test data in db

--- demo/management/commands/setup_test_data.py
+++ demo/management/commands/setup_test_data.py
@@ -7,5 +7,12 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> str | None:
         call_command("makemigrations")
         call_command("migrate")
-        call_command("loaddata", "db_category_fixture.json")
+        call_command("loaddata", "db_categories_fixture.json")
         call_command("loaddata", "db_admin_user_fixture.json")
+        call_command("loaddata", "db_brands_fixture.json")
+        call_command("loaddata", "db_products_fixture.json")
+        call_command("loaddata", "db_labels_fixture.json")
+        call_command("loaddata", "db_product_types_fixture.json")
+        call_command("loaddata", "db_product_attributes_fixture.json")
+        call_command("loaddata", "db_product_attribute_values_fixture.json")
+        call_command("loaddata", "db_product_items_fixture.json")
