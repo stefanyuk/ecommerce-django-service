62630c6463b0dd6349bf27d2e96675a7f3b5ce0a
sstefanyuk <stefanyuk.andriyigor@gmail.com>
2023-08-25 13:02:48 +0200

    updated category model

--- categories/models.py
+++ categories/models.py
@@ -5,6 +5,11 @@ class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
     parent_category = models.ForeignKey('self', null=True, blank=True, on_delete=models.PROTECT)
     is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
